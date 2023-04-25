0038cc91c45eac9b5dfa06326aa119ebdc0a0ae6
Blond Lion <blondlion@gmail.com>
2023-04-25 07:12:23 -0400

    Added Indexdealer and API

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,19 @@
+from django.db import models
+from django.core import serializers
+from django.utils.timezone import now
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
+from .models import CarDealer, CarMake, CarModel
 # from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
+import uuid
 import json
 
 # Get an instance of a logger
@@ -43,11 +49,24 @@ def contact(request):
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+#def get_dealerships(request):
+#    context = {}
+#    if request.method == "GET":
+#        return render(request, 'djangoapp/index.html', context)
 
+def get_dealerships(request):
+    if request.method == "GET":
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/ec674888-06e4-4ff4-8ddb-ecc6db00b252/dealership-package/get-dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        context = {}
+        context["dealership_list"] = dealerships
+        
+        return render (request, 'djangoapp/index.html', context)
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        #return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
